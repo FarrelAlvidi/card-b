3e1227e01818996c667ade807821ba161927ed48
Heddi Heryadi <heddiheryadi@gmail.com>
2025-03-01 16:48:51 +0700

    music

--- script.js
+++ script.js
@@ -43,22 +43,5 @@ gsap.from("#four", {
 });
 
 
-window.addEventListener("load", () => {
-  Swal.fire({
-    title: "Do you want to play music in the background?",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Yes",
-    cancelButtonText: "No",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      document.querySelector(".song").play();
-      animationTimeline();
-    } else {
-      animationTimeline();
-    }
-  });
-});
+
 
